35c06313f6358127bdc9472ba7ac5e97a38fe6a9
adminssh-add <your-email@example.com>
2023-10-17 13:55:31 +0530

    new changes

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -2,7 +2,9 @@ import { Sequelize } from 'sequelize-typescript';
 import { Account } from 'src/account/entities/account.entity';
 import { Auth } from 'src/auth/entities/auth.entity';
 import { BeatPlan } from 'src/beat-order/entities/beat-order.entity';
+import { Invoice } from 'src/invoices/entities/invoice.entity';
 import { Order } from 'src/order/entities/order.entity';
+import { Payment } from 'src/payments/entities/payment.entity';
 import { Product } from 'src/products/entities/product.entity';
 import { Visit } from 'src/visit/entities/visit.entity';
 
@@ -15,11 +17,11 @@ export const databaseProviders = [
                 host: 'localhost',
                 port: 3306,
                 username: 'root',
-                password: 'password',
+                password: 'Valar@1234',
                 database: 'fingertipplus',
                 logging: false
             });
-            sequelize.addModels([Account, Auth, Product, Order, BeatPlan, Visit]);
+            sequelize.addModels([Account, Auth, Product, Order, BeatPlan, Visit, Invoice, Payment]);
             await sequelize.sync({ alter: true });
             return sequelize;
         },
